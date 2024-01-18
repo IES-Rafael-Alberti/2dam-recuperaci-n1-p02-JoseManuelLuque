df0a5c63f07338298257dfc2024159b517075c14
José Manuel Luque <jluqgon214@g.educaand.es>
2024-01-18 18:15:48 +0100

    Casi Funciona

--- app/src/main/java/com/jluqgon214/cartamasaltarecuperacion/data/Naipes.kt
+++ app/src/main/java/com/jluqgon214/cartamasaltarecuperacion/data/Naipes.kt
@@ -1,5 +1,17 @@
 package com.jluqgon214.cartamasaltarecuperacion.data
 
 enum class Naipes {
-    As, Dos, Tres, Cuatro, Cinco, Seis, Siete, Ocho, Nueve, Diez, Jota, Reina, Rey
+    As,
+    Dos,
+    Tres,
+    Cuatro,
+    Cinco,
+    Seis,
+    Siete,
+    Ocho,
+    Nueve,
+    Diez,
+    Jota,
+    Reina,
+    Rey
 }
